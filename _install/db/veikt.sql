76d70960f005ab512a2ceb00ffbeb2aab6af3f0f
Paulius Mačernis <sugalvojau@gmail.com>
2016-08-17 17:58:47 -0400

    Some DB changes. It is ok for now. However, I think sql dump should be not needed in the future.. Doctrine or other ORM should be used instead. Will see later.

--- _install/db/veikt.sql
+++ _install/db/veikt.sql
@@ -30,6 +30,24 @@ CREATE TABLE IF NOT EXISTS `testing` (
 INSERT INTO `testing` (`name`) VALUES
 ('Test');
 
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE `job` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`project` VARCHAR(50) NULL DEFAULT NULL COMMENT 'Name of the project. The same as code directory, for example: \'cvbankas.lt\'',
+	`idInSourceSystem` TEXT NULL COMMENT 'Id of the job ad in the source system, for example id of a job ad in cvbankas.lt could look like: "1-4268463"',
+	`html` MEDIUMTEXT NULL COMMENT 'Job ad in raw HTML format. Just put here as found.',
+	`url` VARCHAR(2083) NULL DEFAULT NULL COMMENT 'URL of job ad. The URL points to the original job.',
+	`statistics` MEDIUMTEXT NULL COMMENT 'Statistics related to the job ad. Raw HTML format. Just put here as found. Statistics are not part of html column.',
+	`created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When the entry was added to this DB',
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
